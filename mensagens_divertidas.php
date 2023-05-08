23219cd951376db8174fa58caaa391e27bde0327
Judson Paiva <judson.paiva@hotmail.com>
2023-05-08 21:41:24 +0100

    imprimir array, uso do print_r e var_dump - revisao

--- mensagens_divertidas.php
+++ mensagens_divertidas.php
@@ -23,6 +23,16 @@
 			// $lista_de_alunos = array(0=>"Judson Paiva", 1=>"Quissanga Paiva", 2=> "Leonor José");
 			// echo $lista_de_alunos[0];
 			// echo $lista_de_alunos[1];
+
+			$mensagens = array(
+    			"A simplicidade é a maior sofisticação. - Leonardo da Vinci",
+    			"Não importa quão devagar você vá, desde que você não pare. - Confúcio",
+    			"Não existe um caminho para a felicidade. A felicidade é o caminho. - Buda",
+    			"O sucesso é ir de fracasso em fracasso sem perder entusiasmo. - Winston Churchill",
+    			"A educação é a arma mais poderosa que você pode usar para mudar o mundo. - Nelson Mandela");
+			
+			print_r($mensagens);
+			var_dump($mensagens);
 		?>
 		
 
