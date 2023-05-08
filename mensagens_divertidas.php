141c4ad91e3f5cefe4fb751575c00fdbc9c205bc
Judson Paiva <judson.paiva@hotmail.com>
2023-05-08 21:13:44 +0100

    variaveis em PHP - revisao

--- mensagens_divertidas.php
+++ mensagens_divertidas.php
@@ -7,15 +7,22 @@
 	</head>
 
 	<body>
+		<h1>Testando</h1>
+		<?php
+			echo "Olá mundo!";
+			print '<p>Teste de impressao com Print</p>';
+
+			$nome = "Judson Paiva";
+			$idade = 28;
+			$preco_do_pc = 500000;
+			$vender_pc = false;
+		?>
 		
+		
+
 		<?php
 			echo 'Teste tag Padrao';
 		?>
-
-		<?=
-			echo "Teste tag de impreessao";
-		?>
-
 
 	</body>
 </html>
