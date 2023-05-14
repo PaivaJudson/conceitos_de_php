a7c5616202c120163c3001310f8f6b2dbb099564
Judson Paiva <judson.paiva@hotmail.com>
2023-05-14 19:49:27 +0100

    criando e utilizando funcoes - validacao de login eliminando erros

--- exemplos/funcoes_validar_login.php
+++ exemplos/funcoes_validar_login.php
@@ -2,13 +2,12 @@
 
    function validar_login($user, $password){
 
-        $login_bd = 'judson.paiva';
-        $senha_bd = '12345';
+        $login_bd = "judson.paiva";
+        $senha_bd = "12345";
 
         if ($user == $login_bd && $password == $senha_bd) {
             return true;
         }
-
         return false;
    } 
 
