1fac611583f6bf5f03cfcdc96ec84a5d9cef222a
Judson Paiva <judson.paiva@hotmail.com>
2023-05-15 20:55:14 +0100

    add de PHP_Data_Types_index.php

--- PHP_Data_Types/index.php
+++ PHP_Data_Types/index.php
@@ -4,41 +4,30 @@
         public $color;
         public $model;
 
-        public function _construct($color, $model){
-            $this->color = $color;
-            $this->model = $model;
-        }
-
         public function info(){
             return "My car is a ".$this->color+" ".$this->model ."!";
         }
 
-    }
+    };
 
-    /*
-    echo "<h1>Objectos</h1>";
-    $myCar = new Car("Vermelho", "Volvo");
-
-    echo $myCar->info();
-    echo "<br>";
-    */
-
-
+ 
     class Pessoa {
 
         public $nome = null;
         public $idade = 0;
 
-        public function apresentar(){
-            echo "Olá, meu nome é ". this->$nome ." e tenho: ". $this.idade ." de idade";
+        function apresentar(){
+            return "Olá, meu nome é ". this->$nome ." e tenho: ". $this.idade ." de idade";
         }
 
-    }
+    };
 
+
+    
     $pUm = new Pessoa();
     $pUm->$nome = "Judson";
     $pUm->$idade = 10;
 
-    $pUm->apresentar();
-
+    echo "$pUm->apresentar()";
+    
 ?> 
