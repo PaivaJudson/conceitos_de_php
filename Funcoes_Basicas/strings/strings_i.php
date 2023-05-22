9b8472ea6ac8360eaa759a06c2575642cb990da2
Judson Paiva <judson.paiva@hotmail.com>
2023-05-22 20:36:10 +0100

    funcoes

--- Funcoes_Basicas/strings/strings_i.php
+++ Funcoes_Basicas/strings/strings_i.php
@@ -1,8 +1,9 @@
 <?php
 
-    $texto = "Curso Completo de PHP <br>";
+    $texto = "Curso Completo de PHP 12.40<br>";
 
     echo strtolower($texto);
     echo strtoupper($texto);
-
+    echo strlen($texto);
+    echo str_replace(".", ",", $texto);
 ?>
