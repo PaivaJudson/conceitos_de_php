8d8492a9fa4e6c2d0de855efc1a6b1e3a553c86a
Judson Paiva <judson.paiva@hotmail.com>
2023-06-02 23:41:29 +0100

    leitura em arquivos de texto

--- Estudo_de_Caso/Caso_Tres/arquivo.php
+++ Estudo_de_Caso/Caso_Tres/arquivo.php
@@ -3,6 +3,13 @@
 $file = fopen("texto.txt", 'r');
 
 while(($linha = fgets($file, 4096)) != false){
-    echo $linha . "<br />";
+    //echo $linha . "<br />";
 }
 
+// existem outras funcoes como file_get_contents
+
+$arquivo = file('texto.txt');
+
+foreach($arquivo as $linha){
+    echo $linha . " <br />";
+}
